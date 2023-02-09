30093c5a5a4d5756057ca6f8bede8dea8bd72295
Romain Laforge <rlaforge@3F6.42angouleme.fr>
2023-02-09 14:04:27 +0100

    strafing movement good

--- src/movement.c
+++ src/movement.c
@@ -6,10 +6,9 @@
 /*   By: rlaforge <rlaforge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 23:59:47 by rlaforge          #+#    #+#             */
-/*   Updated: 2023/02/09 11:55:05 by rlaforge         ###   ########.fr       */
+/*   Updated: 2023/02/09 13:59:10 by rlaforge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "cub3D.h"
 
@@ -28,11 +27,11 @@ void	move_player(int multi, t_player *player, char **map)
 void	strafe_player(int multi, t_player *player, char **map)
 {
 	if (map[(int)player->posY]
-		[(int)(player->posX + (player->dirX * MOVESPEED * multi))] != '1')
-		player->posY += (player->dirX * MOVESPEED) * multi;
-	if (map[(int)(player->posY + (player->dirY * MOVESPEED * multi))]
+		[(int)(player->posX + (player->planeX * MOVESPEED * multi))] != '1')
+		player->posX += (player->planeX * MOVESPEED) * multi;
+	if (map[(int)(player->posY + (player->planeY * MOVESPEED * multi))]
 		[(int)player->posX] != '1')
-		player->posX += (player->dirY * MOVESPEED) * multi;
+		player->posY += (player->planeY * MOVESPEED) * multi;
 	printf("\e[2AposX:%lf             \n", player->posX);
 	printf("posY:%lf              \n", player->posY);
 }
@@ -71,9 +70,9 @@ int	inputs(int key, t_mlx *mlx)
 	else if (key == KEY_ARROW_R || key == KEY_E)
 		rotate_player(-1, &mlx->player);
 	else if (key == KEY_A)
-		strafe_player(1, &mlx->player, mlx->map);
-	else if (key == KEY_D)
 		strafe_player(-1, &mlx->player, mlx->map);
+	else if (key == KEY_D)
+		strafe_player(1, &mlx->player, mlx->map);
 	ft_display(mlx);
 	return (0);
 }
