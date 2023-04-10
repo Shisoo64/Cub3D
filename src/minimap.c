c59b7d9756bd110e592f9dc49f3c68d1d3df26f7
Benjamin Chabot <bchabot@3E5.42angouleme.fr>
2023-04-10 18:00:30 +0200

    best minimap

--- src/minimap.c
+++ src/minimap.c
@@ -12,20 +12,6 @@
 
 #include "../includes/cub3D.h"
 
-void	draw_background(t_mlx *mlx, int pad_x, int pad_y)
-{
-	int	y;
-	int	x;
-
-	y = -1;
-	while (++y <= mlx->map_y * 5)//WIN_H / 2)
-	{
-		x = -1;
-		while (++x <= mlx->map_x * 6) //WIN_W / 5)
-            my_mlx_pixel_put(&mlx->display, x + pad_x, y + pad_y, 0xbbb8b2);
-	}
-}
-
 void draw_square(t_mlx *mlx, int x, int y, int color)
 {
 	int	i;
@@ -44,69 +30,79 @@ void draw_square(t_mlx *mlx, int x, int y, int color)
 	}
 }
 
+void	draw_background(t_mlx *mlx, int pad_x, int pad_y)
+{
+	int	y;
+	int	x;
+
+	y = -1;
+	while (++y < 21 * 6)//WIN_H / 2)
+	{
+		x = -1;
+		while (++x < 10 * 6) //WIN_W / 5)
+			draw_square(mlx, x + pad_x, y + pad_y, 0xffffff);
+			//my_mlx_pixel_put(&mlx->display, x + pad_x, y + pad_y, 0xffffff);
+	}
+}
+
 void fill_minimap(t_mlx *mlx, int pad_x, int pad_y)
 {
 	int y;
 	int x;
-    // Loop through each cell in the map matrix*
+	int x_screen;
+	int y_screen;
+	// Loop through each cell in the map matrix*
+	y_screen = 0;
+
 	y = mlx->player.posY - 10;
 	if (y < 0)
-		y = 0;
-    while (mlx->map[y] && y < mlx->player.posY + 10)
 	{
-		x = mlx->player.posX - 10;
+		y_screen = -y;
+		y = 0;
+	}
+	while (mlx->map[y] && y < mlx->player.posY + 10)
+	{
+		x = mlx->player.posX - 5;
+		x_screen = 0;
 		if (x < 0)
+		{
+			x_screen = -x;
 			x = 0;
-        while (mlx->map[y][x] && x < mlx->player.posX + 10)
+//			draw_square(mlx, (mlx->player.posX + x_screen) * 6, (mlx->player.posY) * 6, 0x000000);
+		}
+//	else if (y < 0)
+//		draw_square(mlx, (mlx->player.posX) * 6, (mlx->player.posY + y_screen) * 6, 0x000000);
+//	else if (y < 0 && x < 0)
+//		draw_square(mlx, (mlx->player.posX + x_screen) * 6, (mlx->player.posY + y_screen) * 6, 0x000000);
+//	else
+//		draw_square(mlx, (mlx->player.posX) * 6, (mlx->player.posY) * 6, 0x000000);
+		while (mlx->map[y][x] && x < mlx->player.posX + 5)
 		{
 			if (x > mlx->map_x)
 				x = mlx->map_x;
 			// Determine the color to use based on the cell value
 			if (mlx->map[y][x] == '1')
-				draw_square(mlx, x * 6 + pad_x, y * 6 + pad_y, 0x000000);
-			else if (mlx->map[y][x] > 32)
-				draw_square(mlx, x * 6 + pad_x, y * 6 + pad_y, 0xffffff);
-			//my_mlx_pixel_put(&mlx->display, x + pad_x, y + pad_y, color);
+				draw_square(mlx, x_screen * 6 + pad_x, y_screen * 6 + pad_y, 0x000000);
+			else
+				draw_square(mlx, x_screen * 6 + pad_x, y_screen * 6 + pad_y, 0xffffff);
+			x_screen++;
 			x++;
-        }
+		}
 		y++;
-		if (y > mlx->map_y)
-			y = mlx->map_y;
-    }
-}
-
-void	draw_line(t_mlx *mlx, int beginX, int beginY, int endX, int endY, int color)
-{
-	double deltaX = endX - beginX; // 10
-	double deltaY = endY - beginY; // 0
-
-	int pixels = sqrt((deltaX * deltaX) + (deltaY * deltaY));
-	double pixelX = beginX;
-	double pixelY = beginY;
-	while (pixels)
-	{
-		draw_square(mlx, pixelX, pixelY, color);
-		//printf(" pixelX is %f\n", pixelX);
-		//printf(" pixelY is %f\n", pixelY);
-		//my_mlx_pixel_put(&mlx->display, pixelX, pixelY, color);
-		pixelX += deltaX;
-		pixelY += deltaY;
-		--pixels;
+		y_screen++;
 	}
 }
 
-void    draw_minimap(t_mlx *mlx, t_raycast *ray)
+void    draw_minimap(t_mlx *mlx)
 {
 	int	pad_x;
 	int	pad_y;
-	
+
 	pad_x = 0;
 	pad_y = 0;
 
-	(void)ray;
-   	draw_background(mlx, pad_x, pad_y);
+	draw_background(mlx, pad_x, pad_y);
 	fill_minimap(mlx, pad_x, pad_y);
-    draw_square(mlx, (mlx->player.posX * 6) + pad_x, (mlx->player.posY * 6) + pad_y, 0x7EC0EE);
-	//draw_line(mlx, (mlx->player.posX * 6) + pad_x, (mlx->player.posY * 6) + pad_y, (mlx->player.posX * 6) + pad_x + ray->sideDistX, (mlx->player.posY * 6) + pad_y + ray->sideDistY, 0x00a000);
-	//draw_square(mlx, ray-> * 6 + pad_x, ray->raydirX * 6 + pad_y, 0x000000);
+	draw_square(mlx, (5 * 6), (10 * 6), 0x7EC0EE);
+	//draw_square(mlx, (mlx->player.posX * 6) + pad_x, (mlx->player.posY * 6) + pad_y, 0x7EC0EE);
 }
